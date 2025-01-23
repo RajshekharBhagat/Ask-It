45724694693075ab1eba87ead426c3f490679ca1
Rajshekhar Bhagat <rajbhagat27889@gmail.com>
2025-01-23 20:49:01 +0530

    minor changes

--- src/app/api/acceptMessages/route.ts
+++ src/app/api/acceptMessages/route.ts
@@ -93,7 +93,7 @@ export async function GET(req: Request) {
     return Response.json(
       {
         success: true,
-        isAcceptingMessage: foundUser.isAcceptingMessages,
+        isAcceptingMessages: foundUser.isAcceptingMessages,
       },
       {
         status: 200,
